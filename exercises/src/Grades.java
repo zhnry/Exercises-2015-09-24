c4bfb40afc6dfdf68822b503cbef42550aeeddda
phil.oconnell <phil.oconnell@YAR1013-MJ858AF.americas.epicor.net>
2015-09-24 01:19:33 -0400

    Initial

---
+++ exercises/src/Grades.java
@@ -0,0 +1,28 @@
+/*
+ * Repeatedly prompt the user to enter a grade.
+ * 
+ * The grade must be between 0 and 100; or it can be equal to -1.
+ * Entering a -1 means the user has finished entering grades.
+ * 
+ * Output to the user
+ * - How many grades the user entered
+ * - The average of the grades
+ * - Error messages when appropriate
+ * 
+ * Sample interaction:
+ * 
+ * Enter a grade: 70
+ * Enter a grade: 75
+ * Enter a grade: 101
+ * Error. Grade must be between 0 and 100; or -1 to finish
+ * Enter a grade: -1
+ * 
+ * You entered 2 grades. The average is 72.5
+ */
+
+public class Grades {
+  public static void main(String[] args) {
+    
+  }
+
+}
